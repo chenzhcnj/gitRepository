2eaf3ca17bc3eaacfb7ceee6b4b9855e905a43ec
chenzc <chenzhcnj@gmail.com>
2018-08-02 23:28:39 +0800

    Modify Hello.java

--- Hello.java
+++ Hello.java
@@ -1,5 +1,6 @@
 public class Hello {
 	public static void main(String[] args) {
-		System.out.println("世界 你好!我们");
+		System.out.println("HAPPY NEW YEAR");
+		System.out.println("HAPPY MYSELF");
 	}
 }
