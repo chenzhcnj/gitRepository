f12bcda74a0e81130d8bfbf6263c8e51a25c7f7e
chenzc <chenzhcnj@gmail.com>
2018-08-05 14:56:09 +0800

    Hello.java添加一个方法method

--- Hello.java
+++ Hello.java
@@ -2,5 +2,11 @@ public class Hello {
 	public static void main(String[] args) {
 		System.out.println("HAPPY NEW YEAR");
 		System.out.println("HAPPY MYSELF");
+		print();
+	}
+	public static void print() {
+		System.out.println("**************************");
+		System.out.println("* Hello World *");
+		System.out.println("**************************");
 	}
 }
