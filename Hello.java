c805a3116ea2268c64710231daf53cf4c06de0fc
chenzc <chenzhcnj@gmail.com>
2018-08-05 15:41:29 +0800

    修改Hello的输出

--- Hello.java
+++ Hello.java
@@ -1,8 +1,8 @@
 public class Hello {
 	public static final  TEST = "111222";
 	public static void main(String[] args) {
-		System.out.println("HAPPY NEW YEAR");
-		System.out.println("HAPPY MYSELF");
+		System.out.println("Hello World");
+		System.out.println("Hello friend");
 		print();
 	}
 	public static void print() {
